b82447be485f8c5a172e731dcb14b33a4f3a5919
clark1816 <33646776+clark1816@users.noreply.github.com>
2021-11-23 08:58:46 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+#auth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET)
+#auth.set_access_token(config.TWITTER_ACCESS_TOKEN, config.TWITTER_ACCESS_TOKEN_SECRET)
+zendeskurl = 'https://zendeskcodingchallenge2053.zendesk.com/api/v2/tickets.json'
+user = 'camst149@mail.rmu.edu'
+pwd = 'Cman3742!'
